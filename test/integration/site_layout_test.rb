e614e70871601836848f38ec9566900460a4e119
Raymond Belonia <raymondbelonia@gmail.com>
2014-10-15 13:50:35 +0800

    Added test for the sign up page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,6 +5,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do 
     get root_path
     assert_template 'static_pages/home'
+    get signup_path
+    assert_select "title", "Ruby on Rails Tutorial Sample App | Sign up"
     assert_select "a[href=?]", root_path
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
